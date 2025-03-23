baffacebd6782e51afd39b680badce244a47d3e8
Shishatka <Shishatka@gmail.com>
2025-03-23 13:45:10 +0300

    Initial commit

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,8 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static Employee[] employees = new Employee[10];
-    public static void main(String[] args) {
+public class EmployeeBook {
+    private Employee[] employees;
+
+    public EmployeeBook(){
+        employees = new Employee[20];
         employees[0] = new Employee("Иванов Иван Иванович", 1, 50000);
         employees[1] = new Employee("Иванов Иван Иванович", 1, 50000);
         employees[2] = new Employee("Сидоров Сидор Сидорович", 3, 70000);
@@ -13,48 +13,25 @@ public class Main {
         employees[7] = new Employee("Алексеева Екатерина Андреевна", 3, 75000);
         employees[8] = new Employee("Дмитриев Дмитрий Дмитриевич", 4, 65000);
         employees[9] = new Employee("Федорова Мария Ивановна", 5, 85000);
-
-        printAll();
-        calculateAll();
-        findMinimumSalary();
-        findMaximumSalary();
-        calculateMean();
-        printNames();
-        //indexAllSalaries(150);
-        printAll();
-
-        findMinSalaryInDepartment(3);
-        findMaxSalaryInDepartment(3);
-        findMeanInDepartment(3);
-
-        indexInDepartment(5, 150);
-        printAllInDepartment(5);
-        System.out.println();
-        printAllLessThan(90_000);
-        System.out.println();
-        printAllMoreThan(50_000);
-        System.out.println(employees[0].equals(employees[1]));
-
-
-
-
-
-
-        EmployeeBook book = new EmployeeBook();
-        book.addNewEmployee(employees[0]);
-        book.addNewEmployee(employees[0]);
-        book.addNewEmployee(employees[0]);
-        book.printAll();
-
     }
 
-    public static void printAll() {
+    public boolean addNewEmployee (Employee emp) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = emp;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printAll() {
         for (int i = 0; i < employees.length; i++) {
             System.out.println(employees[i]);
         }
     }
 
-    public static void calculateAll() {
+    public void calculateAll() {
         double totalAmount = 0;
         for (int i = 0; i < employees.length; i++) {
             totalAmount = totalAmount + employees[i].getSalary();
@@ -62,7 +39,7 @@ public class Main {
         System.out.println("Сумма ЗП в месяц - " + totalAmount);
     }
 
-    public static void findMinimumSalary() {
+    public void findMinimumSalary() {
         double min = Integer.MAX_VALUE;
         for (int i = 0; i < employees.length; i++) {
             if (min > employees[i].getSalary())
@@ -71,7 +48,7 @@ public class Main {
         System.out.println("Минимальная ЗП в месяц - " + min);
     }
 
-    public static void findMaximumSalary() {
+    public void findMaximumSalary() {
         double min = Integer.MIN_VALUE;
         for (int i = 0; i < employees.length; i++) {
             if (min < employees[i].getSalary())
@@ -80,7 +57,7 @@ public class Main {
         System.out.println("Максимальная ЗП в месяц - " + min);
     }
 
-    public static void calculateMean() {
+    public void calculateMean() {
         double totalAmount = 0;
         for (int i = 0; i < employees.length; i++) {
             totalAmount = totalAmount + employees[i].getSalary();
@@ -88,26 +65,25 @@ public class Main {
         System.out.println("Средняя ЗП в месяц - " + totalAmount / employees.length);
     }
 
-    public static void printNames() {
+    public void printNames() {
         for (int i = 0; i < employees.length; i++) {
             System.out.println(employees[i].getName());
         }
     }
 
-    public static void indexAllSalaries(double percent) {
+    public void indexAllSalaries(double percent) {
         for (int i = 0; i < employees.length; i++) {
             employees[i].setSalary(employees[i].getSalary() * (percent / 100));
         }
     }
 
-    public static void findMinSalaryInDepartment(int department){
+    public void findMinSalaryInDepartment(int department) {
         if (department < 0) {
             throw new IllegalArgumentException();
         }
         double min = Integer.MAX_VALUE;
         for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getDepartment() == department)
-            {
+            if (employees[i].getDepartment() == department) {
                 if (min > employees[i].getSalary()) {
                     min = employees[i].getSalary();
                 }
@@ -116,14 +92,13 @@ public class Main {
         System.out.println(min);
     }
 
-    public static void findMaxSalaryInDepartment(int department){
+    public void findMaxSalaryInDepartment(int department) {
         if (department < 0) {
             throw new IllegalArgumentException();
         }
         double min = Integer.MIN_VALUE;
         for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getDepartment() == department)
-            {
+            if (employees[i].getDepartment() == department) {
                 if (min < employees[i].getSalary()) {
                     min = employees[i].getSalary();
                 }
@@ -132,7 +107,7 @@ public class Main {
         System.out.println(min);
     }
 
-    public static void findMeanInDepartment(int department) {
+    public void findMeanInDepartment(int department) {
         if (department < 0) {
             throw new IllegalArgumentException();
         }
@@ -144,10 +119,10 @@ public class Main {
                 totalAmount += employees[i].getSalary();
             }
         }
-        System.out.println(totalAmount/j);
+        System.out.println(totalAmount / j);
     }
 
-    public static void indexInDepartment(int department, double percent) {
+    public void indexInDepartment(int department, double percent) {
         if (department < 0) {
             throw new IllegalArgumentException();
         }
@@ -158,7 +133,7 @@ public class Main {
         }
     }
 
-    public static void printAllInDepartment(int department) {
+    public void printAllInDepartment(int department) {
         if (department < 0) {
             throw new IllegalArgumentException();
         }
@@ -169,7 +144,7 @@ public class Main {
         }
     }
 
-    public static void printAllLessThan(double salary) {
+    public void printAllLessThan(double salary) {
         if (salary < 0) {
             throw new IllegalArgumentException();
         }
@@ -180,7 +155,7 @@ public class Main {
         }
     }
 
-    public static void printAllMoreThan(double salary) {
+    public void printAllMoreThan(double salary) {
         if (salary < 0) {
             throw new IllegalArgumentException();
         }
